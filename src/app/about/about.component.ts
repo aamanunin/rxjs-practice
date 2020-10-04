f80cbc8757c24613ac413af290e920653ea2ddab
Aleksandr Maniunin <aamanunin@gmail.com>
2020-10-04 18:30:30 +0700

    add simple Observable

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {fromEvent, interval, timer} from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -7,9 +8,25 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    // const interval$ = interval(1000);
+    const interval$ = timer(3000, 1000);
+
+
+    const subscription = interval$.subscribe(value => console.log('stream 1 ' + value));
+
+    setTimeout(() => subscription.unsubscribe(), 5000);
+
+    const click$ = fromEvent(document, 'click');
+
+    click$.subscribe(
+      event => console.log(event),
+      error => console.log(error),
+      () => console.log('complete')
+    );
   }
 
 }
